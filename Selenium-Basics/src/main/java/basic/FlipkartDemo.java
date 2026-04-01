cc6320e6ec44100f6ea549055f9c8bb16bad0da1
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-04-01 12:38:29 +0530

    Using chrome driver

--- Selenium-Basics/src/main/java/basic/FlipkartDemo.java
+++ Selenium-Basics/src/main/java/basic/FlipkartDemo.java
@@ -1,13 +1,14 @@
 package basic;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class FlipkartDemo {
 
     public static void main(String[] args) {
 
-        WebDriver driver = new EdgeDriver();
+        WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
