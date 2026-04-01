252be30d4d7cae38bedcb509eeef1028b5893dae
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-04-01 11:18:44 +0530

    Selenium 1st work

---
+++ Selenium-Basics/src/test/java/basic/FlipkartDemo.java
@@ -0,0 +1,32 @@
+package basic;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FlipkartDemo {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver=new EdgeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.flipkart.com/");
+		
+		try {
+			Thread.sleep(1000);
+		}
+		catch(Exception e) {}
+				
+		WebElement x=driver.findElement(By.className("b3wTlE"));
+		x.click();
+		
+		String title=driver.getTitle();
+		System.out.println("Title : "+title+" Length : "+title.length());
+		
+		String url=driver.getCurrentUrl();
+		System.out.println("Current URL : "+url);
+		
+		String source=driver.getPageSource();
+		System.out.println("Source code length : "+source.length());
+	}
+}
