c315bf5318ac54044d71d99b055089d27641432d
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-04-01 12:42:19 +0530

    Using firefox driver

--- Selenium-Basics/src/main/java/basic/FlipkartDemo.java
+++ Selenium-Basics/src/main/java/basic/FlipkartDemo.java
@@ -3,32 +3,17 @@ package basic;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FlipkartDemo {
 
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
+        WebDriver driver = new FirefoxDriver();
 
         driver.get("https://www.flipkart.com/");
-
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {}
-
-        WebElement x = driver.findElement(By.className("b3wTlE"));
-        x.click();
-
-        String title = driver.getTitle();
-        System.out.println("Title : " + title + " Length : " + title.length());
-
-        String url = driver.getCurrentUrl();
-        System.out.println("Current URL : " + url);
-
-        String source = driver.getPageSource();
-        System.out.println("Source code length : " + source.length());
+        System.out.println("Title: " + driver.getTitle());
+        System.out.println("URL: " + driver.getCurrentUrl());
 
         driver.quit();
     }
